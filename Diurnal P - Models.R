18267e552b2846905c4d86e1090c52f8d122a829
Matt Powers <citizenpowers@hotmail.com>
2020-11-25 12:30:09 -0500

    Created spline model to predict TP deviation from daily mean

--- Diurnal P - Models.R
+++ Diurnal P - Models.R
@@ -1,4 +1,9 @@
 
+library(tidymodels) 
+library(modelr)
+library(caret)
+library(broom)
+library(splines)
 library(forecast)
 library(moderndive)
 library(feasts)
@@ -17,9 +22,80 @@ library(ggpmisc)
 
 
 
+# Import data -------------------------------------------------------------
+
 
 #Import RPA Flow and Stage and weather data
 RPAs_with_Flow_Stage_Weather_Sonde <- read_csv("Data/RPA and Flow Stage Weather Sonde.csv")
+
+
+# Tidy Model Method using spline ------------------------------------------------------------
+
+#remove outliers
+RPA_Tidy_model_data<- RPAs_with_Flow_Stage_Weather_Sonde %>%
+filter(TPO4 < 60,abs(Diff_24_hour_mean) <10 )   #remove right skew, outliers
+
+RPA_Tidy_model_data_hourly_mean <- RPA_Tidy_model_data %>%
+group_by(Station,Hour) %>%
+summarise(`TPO4 mean`=mean(TPO4,na.rm=TRUE),`Avg Deviation from Daily mean`=mean(Diff_24_hour_mean,na.rm=TRUE) )
+
+
+RPA_Tidy_model_data_train <- RPA_Tidy_model_data %>% dplyr::sample_frac(.75)
+RPA_Tidy_model_data_test  <- dplyr::anti_join(RPA_Tidy_model_data, RPA_Tidy_model_data_train, by = c("Station","Date","Hour"))
+
+#spline models
+mod1 <- lm(Diff_24_hour_mean ~ ns(Hour, 1), data = RPA_Tidy_model_data_train)
+mod2 <- lm(Diff_24_hour_mean ~ ns(Hour, 2), data = RPA_Tidy_model_data_train)
+mod3 <- lm(Diff_24_hour_mean ~ ns(Hour, 3), data = RPA_Tidy_model_data_train)  #i like model3 the best of spline models
+mod4 <- lm(Diff_24_hour_mean ~ ns(Hour, 4), data = RPA_Tidy_model_data_train)
+mod5 <- lm(Diff_24_hour_mean ~ ns(Hour, 5), data = RPA_Tidy_model_data_train)
+
+grid <- RPA_Tidy_model_data_train  %>% 
+data_grid(Hour = seq_range(Hour, n = 500, expand = 0.1)) %>% 
+gather_predictions(mod1, mod2, mod3, mod4, mod5, .pred = "Diff_24_hour_mean")
+
+#visualize models on trainign data
+ggplot(RPA_Tidy_model_data_train, aes(Hour, Diff_24_hour_mean)) + 
+geom_point() + geom_line(data=RPA_Tidy_model_data_hourly_mean, aes(Hour,`Avg Deviation from Daily mean`), color="blue")+
+geom_line(data = grid, colour = "red") + theme_bw()+
+facet_wrap(~model)
+
+#visualize models on test data
+ggplot(RPA_Tidy_model_data_test, aes(Hour, Diff_24_hour_mean)) + 
+geom_point() + geom_line(data=RPA_Tidy_model_data_hourly_mean, aes(Hour,`Avg Deviation from Daily mean`), color="blue")+
+geom_line(data = grid, colour = "red") + theme_bw()+
+facet_wrap(~ model)
+
+#predict TPO4 using model3
+RPA_Tidy_model_predict <-  RPA_Tidy_model_data %>%
+add_predictions(mod3) %>%
+mutate(date_time=ISOdate(Year,match(Month,month.abb),Day,Hour,Minute,0,tz = "America/New_York"),`Predicted TPO4`=`24_hour_mean`+pred)
+
+#visualize predictions over entire time series
+ggplot(RPA_Tidy_model_predict, aes(date_time,TPO4,color=Station))+geom_point()+facet_wrap(~Station,ncol=1)+theme_bw()+
+geom_point(data=RPA_Tidy_model_predict, aes(date_time,`Predicted TPO4`), color="black")
+
+#visualize predictions over short time 
+ggplot(RPA_Tidy_model_predict, aes(date_time,TPO4,color=Station))+geom_point()+facet_wrap(~Station,ncol=1)+theme_bw()+
+geom_line(data=RPA_Tidy_model_predict, aes(date_time,`Predicted TPO4`), color="black")+scale_x_datetime(limits = ymd_hms(c("2014-07-01 00:00:00", "2014-07-30 00:00:00")))
+
+
+RPA_Tidy_model_data_test_pred <- RPA_Tidy_model_data_test %>%
+add_predictions(mod3) 
+  
+Model_performance <- data.frame(RMSE = RMSE(RPA_Tidy_model_data_test_pred$pred, RPA_Tidy_model_data_test_pred$Diff_24_hour_mean),R2 = R2(RPA_Tidy_model_data_test_pred$pred, RPA_Tidy_model_data_test_pred$Diff_24_hour_mean))
+
+print(Model_performance)
+
+
+
+
+
+
+
+
+
+# Test code ---------------------------------------------------------------
 
 
 
@@ -27,7 +103,7 @@ RPAs_with_Flow_Stage_Weather_Sonde <- read_csv("Data/RPA and Flow Stage Weather 
 RPA_Model_tsibble<- RPAs_with_Flow_Stage_Weather_Sonde %>%
 filter(TPO4 < 60,Date >"2016-02-08",Date <"2017-09-02",abs(Diff_24_hour_mean) <10 ) %>%  #remove right skew, outliers
 mutate(Date_time=ISOdate(year(Date),month(Date),day(Date),Hour,0,0,tz = "America/New_York"))    %>%
-select(Date_time,Station,Diff_24_hour_mean,TPO4)  %>%
+select(Date_time,Station,Diff_24_hour_mean)  %>%
 distinct() %>%      #duplicates in data? follow up on
 as_tsibble(index =Date_time, key = c("Station")) 
 
@@ -38,22 +114,33 @@ RPA_gaps <- RPA_Model_tsibble%>% count_gaps(.full = TRUE)
 ggplot(RPA_gaps, aes(x = Station, colour = Station)) +geom_linerange(aes(ymin = .from, ymax = .to)) +geom_point(aes(y = .from)) +geom_point(aes(y = .to)) +coord_flip() +theme(legend.position = "bottom")
 
 #fill gaps
-RPA_Model_tsibble_full <- RPA_Model_tsibble %>% fill_gaps(.full = TRUE) %>% arrange(Date_time) %>% fill(TPO4,Diff_24_hour_mean)
+RPA_Model_tsibble_full <- RPA_Model_tsibble %>% fill_gaps(.full = TRUE) %>% arrange(Date_time) %>% fill(Diff_24_hour_mean) 
 
 #TPO4
 RPA_Model_tsibble_full %>% gg_season(TPO4,period="day",na.rm=TRUE) + theme(legend.position = "none")
 
-
 #deviation from daily mean
 RPA_Model_tsibble_full %>% gg_season(Diff_24_hour_mean,period="day",na.rm=TRUE) + theme(legend.position = "none")
 
+autoplot(RPA_Model_tsibble_full) +
+autolayer(naive(RPA_Model_tsibble_full, h=11),series="Naïve", PI=FALSE) +
+autolayer(snaive(RPA_Model_tsibble_full, h=11),series="Seasonal naïve", PI=FALSE) 
+
+test3 <-test  %>% forecast(h=50) #%>% autoplot()   #niave
 
 
+fc <- hw(subset(hyndsight,end=length(hyndsight)-35),
+         damped = TRUE, seasonal="multiplicative", h=35)
+  autoplot(hyndsight) +
+  autolayer(fc, series="HW multi damped", PI=FALSE)+
+  guides(colour=guide_legend(title="Daily forecasts"))
+  
+  
+test4  <-  hw(RPA_Model_tsibble_full, damped = TRUE, seasonal="multiplicative", h=50)
 
 
-
-# Test code ---------------------------------------------------------------
-
+test <-naive(RPA_Model_tsibble_full, h=50)
+test1 <-snaive(RPA_Model_tsibble_full, h=11)
 
 
 #Plot of data to be modeled -TP by Station 
