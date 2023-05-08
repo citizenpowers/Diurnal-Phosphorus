c4f8a5663ddab75f4382ee1849740964839d0182
matt powers <citizenpowers@hotmail.com>
2023-05-08 09:29:11 -0400

    cleaned up script for use in diel P manuscript supplemental materials Git Hub Archive

--- Diurnal P - Supplemental Materials.R
+++ Diurnal P - Supplemental Materials.R
@@ -14,19 +14,9 @@ library(scales)     #rescaling function
 
 RPAs_with_Flow_Stage_Weather_Sonde <- read_csv("Data/RPA and Flow Stage Weather Sonde.csv") 
 
-
-STA2_SAV_crash <- RPAs_with_Flow_Stage_Weather_Sonde %>%
-filter(Station=="G334") %>%  
-mutate(Flag=if_else(Station == "G334" & Date >="2017-01-01",TRUE,FALSE)  ) %>%
-count(Flag)
-
-
-
 # Theme for plots ---------------------------------------------------------
 # Create Theme for plots
 themeSize<- theme(text = element_text(family = "serif"),axis.text = element_text(size = 11),axis.title = element_text(size = 11),legend.title = element_text(size = 11),legend.text = element_text(size = 11))
-
-
 
 # Tidy Data ---------------------------------------------------------------
 
@@ -40,11 +30,13 @@ mutate(Station_ID=as.factor(Station_ID),Flowway=as.factor(Flowway),Year=as.facto
 mutate(`Mean_Depth` = case_when(Flowway=="STA-3/4 Central"~`Stage_Out`-9.4,
                                 Flowway=="STA-3/4 Western"~`Stage_Out`-9.7,
                                 Flowway=="STA-2 Central"~`Stage_Out`-9.5)) %>%                #Calculate mean depth using average ground stage
+mutate(`Flowway` = if_else(Flowway=="STA-2 Central","STA-2 Flow-way 3",Flowway)) %>%                #fix flow-way name
+mutate(Flowway=as.factor(Flowway)) %>%  
 filter(HLRout<18,HLRout>0) %>%                                                                #filter out high HLRs that only exist in the STA-2 Central flow-way and reverse flow conditions
 select(TPO4,Flowway,Station_ID,Year,Day,Time,TPO4,HLRout,Mean_Depth,Wind,Rain,Diff_24_hour_mean,`Percent difference from daily mean`) %>%
 mutate(`Label` = case_when(Flowway=="STA-3/4 Central"~"STA-3/4 Central \n (G379D)",
                                   Flowway=="STA-3/4 Western"~"STA-3/4 Western \n (G381B)",
-                                  Flowway=="STA-2 Central"~"STA-2 Flow-way 3 \n (G334)")) %>% 
+                                  Flowway=="STA-2 Flow-way 3"~"STA-2 Flow-way 3 \n (G334)")) %>% 
 drop_na()
   
 #Create training and test data sets
@@ -82,38 +74,38 @@ saveRDS(Mod_1.2, file="./Data/Model/Mod_1.2.rda")
 
 
 #Create Partial effect plot figure 3
-PDP_mod_1.2 <- list()
+PEP_mod_1.2 <- list()
 
 # use c for categorical terms and s for smoothed terms
-PDP_mod_1.2[[1]]<- plot(sm(getViz(Mod_1.2), 1)) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95) +l_rug() +labs(x = expression("Seasonality (Day)")) +
-  theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("g) p-value <0.001")) +theme_classic() +themeSize
+PEP_mod_1.2[[1]]<- plot(sm(getViz(Mod_1.2), 1)) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95) +l_rug() +labs(x = expression("Seasonality (Day)")) +
+theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("g) p-value <0.001")) +theme_classic() +themeSize
 
-PDP_mod_1.2[[2]] <- plot(sm(getViz(Mod_1.2), 2)) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95)+l_points(size = 1, col = "red") +labs(x = expression("Gaussian quantiles (Year)")) +
-  theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("h) p-value <0.001")) +theme_classic() +themeSize
+PEP_mod_1.2[[2]] <- plot(sm(getViz(Mod_1.2), 2)) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95)+l_points(size = 1, col = "red") +labs(x = expression("Gaussian quantiles (Year)")) +
+theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("h) p-value <0.001")) +theme_classic() +themeSize
 
-PDP_mod_1.2[[3]] <- plot(pterm(getViz(Mod_1.2), 1)) + l_fitPoints(size = 1, col = "red")  +l_ciBar(linetype = 3, colour = "blue", level = 0.95)+ l_rug() + labs(x = expression("Flow-way")) +
-  theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("b) p-value <0.001")) +theme_classic() +themeSize
+PEP_mod_1.2[[3]] <- plot(pterm(getViz(Mod_1.2), 1)) + l_fitPoints(size = 1, col = "red")  +l_ciBar(linetype = 3, colour = "blue", level = 0.95)+ l_rug() + labs(x = expression("Flow-way")) +
+theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("b) p-value <0.001")) +theme_classic() +themeSize
 
-PDP_mod_1.2[[4]] <- plot(sm(getViz(Mod_1.2), 3))  +labs(x = expression("Discharge (cm/day)")) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95) +l_rug() +
-  theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("d) p-value <0.001")) +theme_classic() +themeSize
+PEP_mod_1.2[[4]] <- plot(sm(getViz(Mod_1.2), 3))  +labs(x = expression("Discharge (cm/day)")) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95) +l_rug() +
+theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("d) p-value <0.001")) +theme_classic() +themeSize
 
-PDP_mod_1.2[[5]] <- plot(sm(getViz(Mod_1.2), 4))  +labs(x = expression("Discharge (cm/day)")) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95) +l_rug() +
-  theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("e) p-value <0.001")) +theme_classic() +themeSize
+PEP_mod_1.2[[5]] <- plot(sm(getViz(Mod_1.2), 4))  +labs(x = expression("Discharge (cm/day)")) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95) +l_rug() +
+theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("e) p-value <0.001")) +theme_classic() +themeSize
 
-PDP_mod_1.2[[6]] <- plot(sm(getViz(Mod_1.2), 5))  +labs(x = expression("Discharge (cm/day)")) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95) +l_rug() +
-  theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("f) p-value <0.001")) +theme_classic() +themeSize
+PEP_mod_1.2[[6]] <- plot(sm(getViz(Mod_1.2), 5))  +labs(x = expression("Discharge (cm/day)")) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95) +l_rug() +
+theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("f) p-value <0.001")) +theme_classic() +themeSize
 
-PDP_mod_1.2[[7]] <- plot(sm(getViz(Mod_1.2), 6)) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95) +l_rug() +labs(x = expression("Water Depth (ft)")) +
-  theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("c) p-value <0.001")) +theme_classic() +themeSize
+PEP_mod_1.2[[7]] <- plot(sm(getViz(Mod_1.2), 6)) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95) +l_rug() +labs(x = expression("Water Depth (ft)")) +
+theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("c) p-value <0.001")) +theme_classic() +themeSize
 
-PDP_mod_1.2[[8]] <- plot(sm(getViz(Mod_1.2), 7)) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95) +l_rug() +labs(x = expression("Time of Day (Hour)")) +  scale_x_continuous(breaks=seq(0,24,3))+
-  theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("a) p-value <0.001")) +theme_classic() +themeSize
+PEP_mod_1.2[[8]] <- plot(sm(getViz(Mod_1.2), 7)) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95) +l_rug() +labs(x = expression("Time of Day (Hour)")) +  scale_x_continuous(breaks=seq(0,24,3))+
+theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("a) p-value <0.001")) +theme_classic() +themeSize
 
 fig_layout_1.2<- matrix(c(8,7,5,1,3,4,6,2), nrow = 4)
 
 png("Figures/Partial Effects Plot Mod_1.2.png", units = "mm", res = 1000, height = 280, width = 210)
 
-grid.arrange(grobs = lapply(PDP_mod_1.2, "[[", "ggObj"), layout_matrix = fig_layout_1.2)
+grid.arrange(grobs = lapply(PEP_mod_1.2, "[[", "ggObj"), layout_matrix = fig_layout_1.2)
 
 dev.off()
 
@@ -130,37 +122,29 @@ gam.check(Mod_1.3,rep=1000)
 concurvity(Mod_1.3)
 saveRDS(Mod_1.3, file="./Data/Model/Mod_1.3.rda")
 
-concurvity(Mod_1.2)
-
-Mod_1.3b <- gam(TPO4 ~s(Day,k=10,bs="cc")+Year+Flowway+ s(HLRout,by = Flowway, m = 2, bs = "tp")+s(Mean_Depth,k=5)+s(Time,by = Flowway, m = 2, bs = "cc"),data =Stage_discharge_data_train ,method="REML",family=Gamma(link="log"),knots=list(Day=c(0, 366),Time=c(0,24)))
-plot(Mod_1.3b, shade = TRUE, pages = 1, scale = 0, seWithMean = TRUE,all.terms=TRUE)
-summary(Mod_1.3b)
-gam.check(Mod_1.3b,rep=1000)
-concurvity(Mod_1.3b)
-
 #create partial effect plot for model 1.3 
 
-PDP_mod_1.3 <- list()
+PEP_mod_1.3 <- list()
 
-PDP_mod_1.3[[1]] <- plot(sm(getViz(Mod_1.3), 7))  +labs(x = expression("Time of Day (Hour)")) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95) +l_rug() +
-  theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("a) p-value <0.001")) +theme_classic() +themeSize
+PEP_mod_1.3[[1]] <- plot(sm(getViz(Mod_1.3), 7))  +labs(x = expression("Time of Day (Hour)")) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95) +l_rug() +
+theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("a) p-value <0.001")) +theme_classic() +themeSize+ scale_x_continuous(breaks=seq(0,24,3))
 
-PDP_mod_1.3[[2]] <- plot(sm(getViz(Mod_1.3), 8))  +labs(x = expression("Time of Day (Hour)")) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95) +l_rug() +
-  theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("b) p-value <0.001")) +theme_classic() +themeSize
+PEP_mod_1.3[[2]] <- plot(sm(getViz(Mod_1.3), 8))  +labs(x = expression("Time of Day (Hour)")) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95) +l_rug() +
+theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("b) p-value <0.001")) +theme_classic() +themeSize+ scale_x_continuous(breaks=seq(0,24,3))
 
-PDP_mod_1.3[[3]] <- plot(sm(getViz(Mod_1.3), 9))  +labs(x = expression("Time of Day (Hour)")) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95) +l_rug() +
-  theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("c) p-value <0.001")) +theme_classic() +themeSize
+PEP_mod_1.3[[3]] <- plot(sm(getViz(Mod_1.3), 9))  +labs(x = expression("Time of Day (Hour)")) + l_fitLine(linetype = 1, colour = "red") +l_ciLine(linetype = 3, colour = "blue", level = 0.95) +l_rug() +
+theme(axis.text.x = element_text(angle = 15, hjust = 1)) +ggtitle(~italic("c) p-value <0.001")) +theme_classic() +themeSize+ scale_x_continuous(breaks=seq(0,24,3))
 
 
 fig_layout_1.3<- matrix(c(1,2,3), nrow = 1)
 
 png("Figures/Partial Effects Plot Mod_1.3.png", units = "mm", res = 1000, height = 90, width = 210)
 
-grid.arrange(grobs = lapply(PDP_mod_1.3, "[[", "ggObj"), layout_matrix = fig_layout_1.3)
+grid.arrange(grobs = lapply(PEP_mod_1.3, "[[", "ggObj"), layout_matrix = fig_layout_1.3)
 
 dev.off()
 
-citation(stats)
+
 # Create Figure 5 ---------------------------------------------------------
 
 
@@ -175,13 +159,6 @@ dev.off()
 png("Figures/HLR_Time_perperctive_plot_G334.png", units = "mm", res = 1000, height = 160, width = 140)
 vis.gam(Mod_1.3, view = c("HLRout", "Time"), plot.type = "persp",theta=45,phi=25,too.far=.03,cond=list(Flowway="STA-2 Central"), ticktype="detailed",color="terrain",type="response",xlab="Discharge (cm/day)",ylab="Time (hour)",zlab=paste(expression(TP~ug/L)))  #3D HLR and station
 dev.off()
-
-
-
-
-
-
-sessionInfo()
 
 
 
@@ -202,7 +179,7 @@ png("Figures/Partial Autocorrelation plot Model 1.3.png", units = "mm", res = 10
 pacf(residuals(Mod_1.3))
 dev.off()
 
-qnorm(0.975)
+
 #GGplot method
 acf <- acf(residuals(Mod_1.3),plot = FALSE)
 acf <- with(acf, data.frame(lag, acf))
